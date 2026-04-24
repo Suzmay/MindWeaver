fdaefbb65200e4048c8b2fbe2be393d48d357ba5
Suzmay <2295665424@qq.com>
2026-04-25 02:21:31 +0800

    恢复，准备重跑worker

--- web/worker-minimal.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request) {
-    return new Response(JSON.stringify({ ok: true, message: "Minimal Worker test" }), {
-      headers: { "Content-Type": "application/json" },
-    });
-  },
-};
